ab5db308a4c2c8c24c8d7a6e63d51d3c6a05c5f5
Angel <qifang_2012@outlook.com>
2021-03-18 11:11:11 +0800

    Initial Commit

---
+++ samples/src/samples/Location.java
@@ -0,0 +1,14 @@
+package samples;
+
+public class Location {
+
+	private String Location;
+
+	public String getLocation() {
+		return Location;
+	}
+
+	public void setLocation(String location) {
+		Location = location;
+	}
+}
